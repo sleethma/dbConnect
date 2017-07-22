06c1aef567d3ab44b47b669f4f021692bfc73e6f
Michael Sleeth <sleeth.ma@gmail.com>
2017-07-22 00:22:15 -0500

    Initial Commit

---
+++ app/src/main/java/com/example/micha/connecttodb/BackgroundTask.java
@@ -0,0 +1,89 @@
+package com.example.micha.connecttodb;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.widget.Toast;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+
+/**
+ * Created by micha on 7/21/2017.
+ */
+
+public class BackgroundTask extends AsyncTask<String, Void, String> {
+
+    Context mContextRegister;
+    BackgroundTask(Context context){
+        this.mContextRegister = context;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+    }
+
+    @Override
+    protected String doInBackground(String... params) {
+
+        //stores path of db php registration script on host
+        //server_IPv4/db_folder/file
+        String reg_url = "http://10.0.0.13/project/register.php";
+
+        //assures register button sent this background call
+        String registerBkgdTaskCheck = params[0];
+        if(registerBkgdTaskCheck.equals("register")){
+            //get params
+            String name = params[1];
+            String password = params[2];
+            String contact = params[3];
+            String country = params[4];
+
+            //establish connection
+            try {
+                URL regUrl = new URL(reg_url);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) regUrl.openConnection();
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.setDoOutput(true);
+                //make object of output stream
+                OutputStream os = httpURLConnection.getOutputStream();
+                BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(os,"UTF-8"));
+
+
+                String data = URLEncoder.encode("name","UTF-8") + "=" + URLEncoder.encode(name, "UTF-8") + "&" +
+                        URLEncoder.encode("password","UTF-8") + "=" + URLEncoder.encode(password, "UTF-8") + "&" +
+                        URLEncoder.encode("contact","UTF-8") + "=" + URLEncoder.encode(contact, "UTF-8") + "&" +
+                        URLEncoder.encode("country","UTF-8") + "=" + URLEncoder.encode(country, "UTF-8");
+
+                bufferedWriter.write(data);
+                bufferedWriter.flush();
+                bufferedWriter.close();
+                os.close();
+                InputStream inputStream = httpURLConnection.getInputStream();
+                inputStream.close();
+
+                return "Registration Successful";
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        super.onPostExecute(result);
+        Toast.makeText(mContextRegister, "Registration Success", Toast.LENGTH_LONG).show();
+    }
+
+
+}
---
+++ app/src/main/java/com/example/micha/connecttodb/Register.java
@@ -0,0 +1,39 @@
+package com.example.micha.connecttodb;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import java.net.URLEncoder;
+
+public class Register extends AppCompatActivity {
+
+    EditText nameEntry, countryEntry, passwordEntry, contactEntry;
+    String name, password, contact, country;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        nameEntry = (EditText) findViewById(R.id.et_name);
+        countryEntry = (EditText) findViewById(R.id.et_country);
+        passwordEntry = (EditText) findViewById(R.id.et_password);
+        contactEntry = (EditText) findViewById(R.id.et_contact);
+    }
+
+    public void registerUser(View view){
+        name = nameEntry.getText().toString();
+        password = passwordEntry.getText().toString();
+        contact = contactEntry.getText().toString();
+        country = countryEntry.getText().toString();
+        String registerBackgroundTask = "register";
+        BackgroundTask backgroundTask = new BackgroundTask(this);
+        backgroundTask.execute(registerBackgroundTask, name, password, contact, country);
+        finish();
+
+
+    }
+
+}
